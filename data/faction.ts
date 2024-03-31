69e778906793fb27681623e73c0dde79412bbade
James Pereira <james.pereira90@gmail.com>
2024-03-31 15:42:59 +1100

    update cache  for faction retrieval

--- data/faction.ts
+++ data/faction.ts
@@ -1,5 +1,6 @@
 "use server";
 import { promises as fs } from "fs";
+import { revalidatePath } from "next/cache";
 
 export const getFactionByName = async (faction: string) => {
   try {
@@ -8,6 +9,8 @@ export const getFactionByName = async (faction: string) => {
       "utf8"
     );
     const data = await JSON.parse(file);
+
+    revalidatePath("/games/");
     return data.catalogue;
   } catch {
     return null;
