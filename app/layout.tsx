601712e4878c01edcc2ccc5accc60e3033b7d825
James Pereira <james.pereira90@gmail.com>
2024-04-14 01:55:28 +1000

    update apple web app

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,7 @@ export const metadata: Metadata = {
   title: "⚔️ Game Night",
   description: "Manage your upcoming game night",
   appleWebApp: {
+    capable: true,
     statusBarStyle: "black-translucent",
   },
 };
