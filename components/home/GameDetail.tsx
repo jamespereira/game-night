fc1c8019259ab5f9deb28efc5ed6570883e80d65
James Pereira <james.pereira90@gmail.com>
2024-09-28 00:54:34 +1000

    update game list update changes

--- components/home/GameDetail.tsx
+++ components/home/GameDetail.tsx
@@ -95,14 +95,14 @@ const GameDetail = async ({ game }: Props) => {
             />
           </CardContent>
           <CardFooter>
-            {!!user && isBeforeGame(game.id) ? (
+            {!!userTeam && isBeforeGame(game.date) ? (
               <div className="flex items-center justify-center">
                 <Button className="bg-amber-400/85 [drop-shadow:_0_0_5px_rgb(0_0_0_/_80%)] text-black">
                   <Link
                     href="/games/[gameId]/teams/[teamId]"
                     as={`/games/${game.id}/teams/${userTeam}`}
                   >
-                    Update Team
+                    Update List
                   </Link>
                 </Button>
               </div>
