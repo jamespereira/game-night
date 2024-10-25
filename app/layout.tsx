56e1aa2a53e78f0b6ced203ee66991cfa7132b45
James Pereira <james.pereira90@gmail.com>
2024-10-25 15:41:52 +1100

    update theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Link from "next/link";
@@ -17,6 +17,10 @@ export const metadata: Metadata = {
   appleWebApp: {
     statusBarStyle: "black-translucent",
   },
+};
+
+export const viewport: Viewport = {
+  themeColor: "#163749",
 };
 
 export default async function RootLayout({
