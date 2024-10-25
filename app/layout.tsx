d4a961d1dad74b6df2a9903b57977096cc909700
James Pereira <james.pereira90@gmail.com>
2024-10-26 01:22:23 +1100

    update font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata, Viewport } from "next";
-import { Inter } from "next/font/google";
+import { Cabin } from "next/font/google";
 import "./globals.css";
 import Link from "next/link";
 import { auth } from "@/auth";
@@ -9,7 +9,7 @@ import Navbar from "@/components/Navbar";
 import { cn } from "@/lib/utils";
 import Image from "next/image";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Cabin({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "⚔️ Game Night",
